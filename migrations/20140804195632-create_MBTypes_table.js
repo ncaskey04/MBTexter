5c67efe26913cd6aad07f40e5d62c1fb0bd949bd
ncaskey04 <ncaskey04@gmail.com>
2014-08-05 09:39:39 -0700

    setup folder structure and ran migrations

---
+++ migrations/20140804195632-create_MBTypes_table.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    // add altering commands here, calling 'done' when finished
+
+    migration.createTable('MBTypes', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+    	createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+      type: DataTypes.STRING,
+      description: DataTypes.TEXT
+    })
+    .complete(done);
+  },
+
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    migration.dropTable('MBTypes')
+    .complete(done);
+  }
+};
