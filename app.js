6fafd29985ab8c189c0fcad7fc5c233b231eb420
ncaskey04 <ncaskey04@gmail.com>
2014-08-07 18:39:31 -0700

    mbtypes table complete

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 // serves static files in public folder, like css
-app.use(express.static(__dirname, '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 // Set up cookieSession: encrypts and names cookies, sets timeout
